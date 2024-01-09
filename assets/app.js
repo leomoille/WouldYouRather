60817c48d320f00614dfc0f9b57265165e823230
Léo Moille <leomoille18@gmail.com>
2024-01-09 05:32:41 +0100

    :tada: start: add some packages and configure docker compose files (#5)

--- assets/app.js
+++ assets/app.js
@@ -5,6 +5,8 @@ import './bootstrap.js';
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
-import './styles/app.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.min.css';
+import './styles/app.css';
 
-console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉')
+console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
